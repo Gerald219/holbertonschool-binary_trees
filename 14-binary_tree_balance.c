0495c34976adf56acb80aad0433960d098073aa5
Gerald Mulero <gdmasm@gmail.com>
2025-04-20 18:24:31 -0400

    fixed it by adding +1 when going down each child, so it can correctly count the height in steps

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,32 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - returns the height of a binary tree
- * @tree: pointer to the root node of the subtree
+ * binary_tree_height - measures height of a binary tree
+ * @tree: pointer to the node to measure from
  * Return: height (number of steps to the deepest leaf)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left = 0, right = 0;
+	size_t left, right;
 
 	if (tree == NULL)
-		return (0);
+		return (0); /* empty tree has height 0 */
 
-	if (tree->left)
-		left = 1 + binary_tree_height(tree->left);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-	if (tree->right)
-		right = 1 + binary_tree_height(tree->right);
-
-	return (left > right ? left : right);
+	return ((left > right ? left : right) + 1);
 }
-
 /**
- * binary_tree_balance - gets the balance factor of a binary tree node
- * @tree: pointer to the node to check
- * Return: balance factor = height of left - height of right
+ * binary_tree_balance - measures balance factor of a node
+ * @tree: pointer to the node
+ * Return: balance factor (left height - right height)
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
